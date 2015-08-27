b4ced3987ef332e35826c37060a1f72ecdc64776
Chintan Shah <chintanshah@promactinfo.com>
2015-08-27 14:03:16 +0530

    updated comoser.yml and setup.sh

--- setup.sh
+++ setup.sh
@@ -12,5 +12,5 @@ git clone -b REL1_25 https://gerrit.wikimedia.org/r/p/mediawiki/extensions/Visua
 cd VisualEditor
 git submodule update –init
 cd /root/docker
-COMPOSE_FILE=docker-compose.yml
+export COMPOSE_FILE=docker-compose.yml
 docker-compose up -d
