e79f604fdb66fc345cd557b2a1ec4526a602b559
Chintan Shah <chintanshah@promactinfo.com>
2015-08-27 18:00:26 +0530

    updated dockerfile for mwlib

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ git clone https://gerrit.wikimedia.org/r/mediawiki/extensions/Collection
 git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/UniversalLanguageSelector.git
 git clone -b REL1_25 https://gerrit.wikimedia.org/r/p/mediawiki/extensions/VisualEditor.git
 cd VisualEditor
-git submodule update –init
+git submodule update –-init
 cd /root/docker
 export COMPOSE_FILE=docker-compose.yml
 docker-compose up -d
